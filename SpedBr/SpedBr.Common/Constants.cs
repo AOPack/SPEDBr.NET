6bce607ee7bcb3ed5d856a69d30e2a7975a75354
Joham Nunes <johamnunes@gmail.com>
2018-02-27 09:33:28 -0300

    [+] Adicionado código da versão da tabela

--- SpedBr/SpedBr.Common/Constants.cs
+++ SpedBr/SpedBr.Common/Constants.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SpedBr.Common
+﻿namespace SpedBr.Common
 {
     public class Constants
     {
         public const decimal VZero = 0.00M;
         public const string StructuralError = "|ERRO_NA_ESTRUTURA|";
+        public const int COD_VER = 012;
     }
 }
