2371a0971be58b022f33673b093cd18003c8d0fa
Joham Nunes <johamnunes@gmail.com>
2018-03-13 16:23:25 -0300

    [*] Ajuste controle

--- SpedBr/SpedBr.Common/Constants.cs
+++ SpedBr/SpedBr.Common/Constants.cs
@@ -1,9 +1,13 @@
-﻿namespace SpedBr.Common
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpedBr.Common
 {
     public class Constants
     {
         public const decimal VZero = 0.00M;
         public const string StructuralError = "|ERRO_NA_ESTRUTURA|";
-        public const int COD_VER = 012;
     }
 }
